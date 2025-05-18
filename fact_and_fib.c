53f83cc1c0fdc21b5a390156a9ad1172dc218642
Ron Barrantes <ronbarrantes@gmail.com>
2025-05-18 19:03:41 -0400

    Add a plan

--- fact_and_fib.c
+++ fact_and_fib.c
@@ -1,1 +1,12 @@
 // factorials and fibonacci in loops and recursion
+// what do I wanna do here
+// maybe I want to run the function and it will give me choices
+// input fac or fib
+// input the number
+//
+// Maybe I can solve factorial with loop
+// and I'll solve fibonacci with recursion
+
+int main() {
+  return 0;
+}
