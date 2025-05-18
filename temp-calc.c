f990abf5894407614282af18196a12017af131b1
Ron Barrantes <ronbarrantes@gmail.com>
2025-05-18 00:33:25 -0400

    Add major fixex

--- temp-calc.c
+++ temp-calc.c
@@ -12,7 +12,7 @@ double ctok(double c) {
 }
 
 /**
- * Celsius to Farenheight
+ * Celsius to Fahrenheit
  * @param c The input double.
  */
 double ctof(double c) {
@@ -29,7 +29,7 @@ double ktoc(double k) {
 }
 
 /**
- * Farenheight to Celsius
+ * Fahrenheit to Celsius
  * @param f The input double.
  */
 double ftoc(double f) {
@@ -38,45 +38,47 @@ double ftoc(double f) {
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    printf("Please pass the temperature with the C|F|K");
+    fprintf(stderr, "Usage: %s <number><C|F|K>\n", argv[0]);
     return 1;
   }
 
-  char *temperature = argv[1];
-  int length = strlen(temperature);
-
-  char degrees = tolower(temperature[length - 1]);
-
+  char *input = argv[1];
   char *end_ptr;
-  double float_temp = strtod(temperature, &end_ptr);
+  double input_temp = strtod(input, &end_ptr);
 
-  // ensure that the degrees is either C or F or K
-  if (degrees != 'c' && degrees != 'f' && degrees != 'k') {
-    printf("Invalid param. The last character should be C|F|K, you inputed %c\n", degrees);
+  // Check that a valid number was parsed
+  if (end_ptr == input || *end_ptr == '\0') {
+    fprintf(stderr, "Invalid input: temperature must end with C, F, or K (e.g., 100C)\n");
+    return 1;
+  }
+
+  char unit = tolower(*end_ptr);
+  if (*(end_ptr + 1) != '\0' || (unit != 'c' && unit != 'f' && unit != 'k')) {
+    fprintf(stderr, "Invalid temperature unit: use C, F, or K\n");
     return 1;
   }
 
   double c, f, k;
 
-  switch (degrees) {
+  switch (unit) {
   case 'c':
-    c = float_temp;
+    c = input_temp;
     f = ctof(c);
     k = ctok(c);
     break;
   case 'f':
-    f = float_temp;
+    f = input_temp;
     c = ftoc(f);
     k = ctok(c);
     break;
   case 'k':
-    k = float_temp;
+    k = input_temp;
     c = ktoc(k);
     f = ctof(c);
 
     break;
   default:
-    printf("ERROR: there was a problem calculating the temperatures\n");
+    fprintf(stderr, "Unknown error occurred\n");
     return 1;
   }
 
